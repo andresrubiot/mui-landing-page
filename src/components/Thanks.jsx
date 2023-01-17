57449fb1e5a2257b43df60438eb4230af7624632
Andres Rubio <andresrubiot5@gmail.com>
2023-01-16 20:03:05 -0500

    Thanks section

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import InstagramIcon from "./InstagramIcon";
+import Button from '@mui/material/Button';
+import { Box, styled } from '@mui/material';
 
-function Thanks() {
+const StyledButton = styled(Button)({
+    background: 'linear-gradient(270deg, #00E6E3 2.73 %, #00FF68 100 %)',
+    borderRadius: '30'
+});
+
+function Benefits() {
     return (
-        <h1>Thanks</h1>
+        <Container maxWidth='md'>
+            <Grid container textAlign='center' my={5}>
+                <Grid item xs={12}>
+                    <Typography variant="h5" component="h5" fontSize={36}>
+                        Gracias por <b className="txt-gradient">completar el ejercicio</b>
+                    </Typography>
+
+                    <Typography variant="p" component="p">
+                        Te invitamos a ver más información
+                    </Typography>
+
+                </Grid>
+            </Grid>
+
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box mx={2}>
+                    <InstagramIcon/>
+                </Box>
+                
+                <a
+                    href="https://wacoservices.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    underline='none'>
+                    <Button className="btn-gradient">
+                        Conocer más
+                    </Button>
+                </a>
+            </Box>
+        </Container>
     )
 }
 
-export default Thanks;
+export default Benefits;
